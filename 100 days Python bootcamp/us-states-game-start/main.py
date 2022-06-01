d50b8debae113ec1bc693b6076835a35e3da0e4d
zjambor <jambor.zoltan1@gmail.com>
2022-06-01 13:28:10 +0200

    ok

--- 100 days Python bootcamp/us-states-game-start/main.py
+++ 100 days Python bootcamp/us-states-game-start/main.py
@@ -15,6 +15,8 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -24,6 +26,20 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
+
+states_to_learn = []
+
+for state in all_states:
+    if state not in guessed_states:
+        states_to_learn.append(state)
+
+new_data = pandas.DataFrame(states_to_learn)
+new_data.to_csv()
+
+# with open('states_to_learn.csv', 'w', newline='') as csvfile:
+#     csvwriter = csv.writer(csvfile, delimiter=',',
+#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#     csvwriter.writerow(states_to_learn)
 
 screen.mainloop()
 
