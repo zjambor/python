15616e787da4785994824320417489858770250a
zjambor <jambor.zoltan1@gmail.com>
2022-08-05 15:32:38 +0200

    ok

---
+++ elearning.training360/functional_programming/closure.py
@@ -0,0 +1,27 @@
+from numpy import inner
+
+
+def outer_fn(n):
+    print('outer_fn')
+    def inner_fn(x):
+        print(f'inner_fn {n}')
+    
+    return inner_fn
+    
+fnx = outer_fn(5)
+del outer_fn
+fnx(3)
+
+# factory pattern
+def pow_make(n):
+    """ calculates pow and returns function"""
+    def pow(x):
+        return x ** n
+
+    return pow
+
+pow_of_2 = pow_make(2)
+pow_of_3 = pow_make(3)
+
+print(pow_of_2(5))
+print(pow_of_3(5))
---
+++ elearning.training360/functional_programming/maps.py
@@ -0,0 +1,18 @@
+from closure import *
+from functools import reduce
+
+li = [1,3,5,2,5,7,8]
+li2 = list(map(pow_of_2, li))
+print(li2)
+
+# higher order func
+
+li3 = list(
+        map(pow_of_2, 
+            filter(lambda x: x % 2 == 0, li)))
+print(li3)
+
+sum2 = reduce(lambda x,y: x+y,
+            map(pow_of_2, 
+                filter(lambda x: x % 2 == 0, li)))
+print(sum2)
