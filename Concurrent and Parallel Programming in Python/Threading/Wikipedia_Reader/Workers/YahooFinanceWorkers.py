3b86912bb9a66dc3216fd3b2c1a3fa8cf71bc534
zjambor <jambor.zoltan1@gmail.com>
2023-09-25 09:27:10 +0200

    ok

--- Concurrent and Parallel Programming in Python/Threading/Wikipedia_Reader/Workers/YahooFinanceWorkers.py
+++ Concurrent and Parallel Programming in Python/Threading/Wikipedia_Reader/Workers/YahooFinanceWorkers.py
@@ -5,7 +5,7 @@ from lxml import html
 class YahooFinanceWorker(threading.Thread):
     def __init__(self, symbol, **kwargs):
         super(YahooFinanceWorker, self).__init__(**kwargs)
-        self._symbol = 'AAPL'
+        self._symbol = symbol
         base_url = 'https://finance.yahoo.com/quote/'
         self._url = f'{base_url}{self._symbol}'
         print(self._url)
@@ -14,7 +14,7 @@ class YahooFinanceWorker(threading.Thread):
     def run(self):
         r = requests.get(self._url)
         if r.status_code != 200:
-            print(r.status_code)
+            print(self._symbol, ': ', r.status_code)
             return
         page_contents = html.fromstring(r.text)
         print(page_contents.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div[1]/fin-streamer[1]'))
